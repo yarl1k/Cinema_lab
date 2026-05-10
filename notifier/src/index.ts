3ce580178b978478d2507c2f7f13f66fd2d91e16
yar4ik <yaroslawdyr@gmail.com>
2026-05-10 16:42:49 +0300

    feat: implemented microservice structure:   - designed code-first db schema for notifier   - updated created dockerFile and updated dockerCompose file to start whole app

---
+++ notifier/src/index.ts
@@ -0,0 +1,45 @@
+import express from 'express';
+import helmet from 'helmet';
+import rateLimit from 'express-rate-limit';
+import { prisma } from './services/prisma_setup/database.js';
+import './services/redis/queue.service.js'; 
+import mainRouter from "./routers/index.js";
+
+const app = express();
+
+app.set('trust proxy', 1);
+app.use(helmet({ contentSecurityPolicy: false }));
+app.use(express.json());
+
+const apiLimiter = rateLimit({
+    windowMs: 15 * 60 * 1000,
+    max: 100,
+    message: 'Too many requests, please try again later',
+});
+app.use('/api', apiLimiter);
+
+app.use('/api', mainRouter);
+
+app.get('/health', (req, res) => {
+    res.status(200).json({ status: 'Notifier is running' });
+});
+
+const PORT = process.env.PORT || 3000;
+
+const startServer = async () => {
+    try {
+        await prisma.$connect();
+        console.log(' Notifier Database connected successfully.');
+
+        app.listen(PORT, () => {
+            console.log(`Notifier Worker started on port ${PORT}`);
+            console.log('BullMQ Ready to process emails');
+        });
+    }
+    catch (error: any) {
+        console.error('Error starting notifier:', error.message || error);
+        process.exit(1);
+    }
+}
+
+startServer();
---
+++ notifier/src/services/prisma_setup/database.ts
@@ -0,0 +1,10 @@
+import 'dotenv/config';
+import pg from "pg";
+import { PrismaPg } from "@prisma/adapter-pg";
+import { PrismaClient } from "../../generated/prisma/client.js";
+const { Pool } = pg;
+const pool = new Pool({ connectionString: process.env.MICROSERVICE_DATABASE_URL });
+
+const adapter = new PrismaPg(pool);
+
+export const prisma = new PrismaClient({ adapter });
